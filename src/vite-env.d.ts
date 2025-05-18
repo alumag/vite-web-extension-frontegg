f58f1a38223c146a383f2f2235e69cee841e62ce
Aluma Gelbard <10659903+alumag@users.noreply.github.com>
2025-05-18 09:10:30 +0300

    Configure .env vars
    Signed-off-by: Aluma Gelbard <10659903+alumag@users.noreply.github.com>

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_PUBLIC_APP_URL: string;
+    readonly VITE_PUBLIC_FRONTEGG_BASE_URL: string;
+    readonly VITE_PUBLIC_FRONTEGG_CLIENT_ID: string;
+    // more env variables...
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
+  
