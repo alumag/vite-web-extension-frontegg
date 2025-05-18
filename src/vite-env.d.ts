3e097d76db2f9f6cee4825ee7d625bc7d416b9e1
Aluma Gelbard <10659903+alumag@users.noreply.github.com>
2025-05-18 09:58:04 +0300

    style
    Signed-off-by: Aluma Gelbard <10659903+alumag@users.noreply.github.com>

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,15 +1,16 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-    /** The AppUrl is used to tell Frontegg your application hostname */
-    readonly VITE_PUBLIC_APP_URL: string;
-    /** The FronteggBaseUrl is used to tell Frontegg your application hostname */
-    readonly VITE_PUBLIC_FRONTEGG_BASE_URL: string;
-    /** The FronteggClientId is used to tell Frontegg your application hostname */
-    readonly VITE_PUBLIC_FRONTEGG_CLIENT_ID: string;
-  }
-  
-  interface ImportMeta {
-    readonly env: ImportMetaEnv;
-  }
-  
+  /** The AppUrl is used to tell Frontegg your application hostname */
+  readonly VITE_PUBLIC_APP_URL: string;
+
+  /** The FronteggBaseUrl is used to tell Frontegg your application hostname */
+  readonly VITE_PUBLIC_FRONTEGG_BASE_URL: string;
+
+  /** The FronteggClientId is used to tell Frontegg your application hostname */
+  readonly VITE_PUBLIC_FRONTEGG_CLIENT_ID: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
